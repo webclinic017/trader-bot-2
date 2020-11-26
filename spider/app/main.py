7e052ccd04ac4e671706ea77f2b79e3fb36aafc2
mfcekirdek <mfcekirdek94@gmail.com>
2020-11-26 22:52:12 +0300

    quantstats library is added to requirements.txt Pyfolio analyzer is added Logging mechanisms

--- spider/app/main.py
+++ spider/app/main.py
@@ -1,5 +1,8 @@
+import datetime
+
 import backtrader as bt
 import backtrader.analyzers as bta
+import quantstats
 
 from app.sizers import LongOnly
 from app.strategies import CloseSMA
@@ -10,15 +13,20 @@ cerebro.broker.setcommission(commission=0.00075)
 # Set data parameters and add to Cerebro
 data = bt.feeds.YahooFinanceCSVData(
     dataname='data/BTC-USD.csv',
+    fromdate=datetime.datetime(2019, 11, 26),
+    todate=datetime.datetime(2020, 11, 26),
 )
 
 cerebro.adddata(data)
 
 cerebro.addstrategy(CloseSMA)
 
-cerebro.addanalyzer(bta.SharpeRatio, _name='mysharpe')
+# cerebro.addanalyzer(bta.SharpeRatio, _name='mysharpe')
+cerebro.addanalyzer(bt.analyzers.PyFolio, _name='PyFolio')
 
 cerebro.addsizer(LongOnly)
+
+cerebro.addwriter(bt.WriterFile, csv=True, out='logs/log.csv')
 
 if __name__ == '__main__':
     start_portfolio_value = cerebro.broker.getvalue()
@@ -29,7 +37,13 @@ if __name__ == '__main__':
     thestrat = thestrats[0]
     pnl = end_portfolio_value - start_portfolio_value
 
-    print('Sharpe Ratio:', thestrat.analyzers.mysharpe.get_analysis())
+    portfolio_stats = thestrat.analyzers.getbyname('PyFolio')
+    returns, positions, transactions, gross_lev = portfolio_stats.get_pf_items()
+    returns.index = returns.index.tz_convert(None)
+    returns.to_csv('logs/returns.csv')
+    quantstats.reports.html(returns, output='logs/stats.html', title='CloseSMA')
+
+    # print('Sharpe Ratio:', thestrat.analyzers.mysharpe.get_analysis())
     print(f'Starting Portfolio Value: {start_portfolio_value:2.2f}')
     print(f'Final Portfolio Value: {end_portfolio_value:2.2f}')
     print(f'PnL: {pnl:.2f}')
--- spider/app/strategies/close_sma.py
+++ spider/app/strategies/close_sma.py
@@ -4,7 +4,13 @@ import backtrader as bt
 class CloseSMA(bt.Strategy):
     params = (('period', 15),)
 
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        print(f'{dt.isoformat()} {txt}')
+        self.log_pnl.append(f'{dt.isoformat()} {txt}')
+
     def __init__(self):
+        self.log_pnl = []
         sma = bt.indicators.SMA(self.data, period=self.p.period)
         self.crossover = bt.indicators.CrossOver(self.data, sma)
 
@@ -14,3 +20,8 @@ class CloseSMA(bt.Strategy):
 
         elif self.crossover < 0:
             self.sell()
+
+    def stop(self):
+        with open('logs/custom_log.csv', 'w') as e:
+            for line in self.log_pnl:
+                e.write(line + '\n')
