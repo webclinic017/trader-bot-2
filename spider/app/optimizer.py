131133b58f9a8fbb79ab308065e0bf1db57434c3
Yasar Budulgan <yb.budulgan@gmail.com>
2020-12-03 20:29:05 +0300

    Update format of optimizer

--- spider/app/optimizer.py
+++ spider/app/optimizer.py
@@ -1,10 +1,12 @@
 import logging
+from copy import deepcopy
 from typing import Type, List
 
 import backtrader as bt
 import pandas as pd
 
 from app.analyzers.acctstats import AcctStats
+from app.timeseriessplit import TimeSeriesSplitImproved
 
 
 class StrategyOptimizer:
@@ -13,9 +15,13 @@ class StrategyOptimizer:
     BINANCE_COMMISSION = 0.00075
     PERCENT_SIZER = 70
 
-    def __init__(self) -> None:
+    def __init__(self, data: pd.DataFrame, symbol: str, interval: str) -> None:
         super().__init__()
         self.cerebro = None
+        self.symbol = symbol
+        self.interval = interval
+        self.dataframe = data
+        self.btdata = bt.feeds.PandasData(dataname=data, datetime='open_time')
         self.__init_cerebro()
 
     def __init_cerebro(self):
@@ -27,20 +33,25 @@ class StrategyOptimizer:
         self.cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name='Basic_Stats')
         self.cerebro.addsizer(bt.sizers.PercentSizer, percents=self.PERCENT_SIZER)
 
-    def run_single(self, data: bt.feed.DataBase, strategy: Type[bt.Strategy], params=None) -> bt.MetaStrategy:
+    def run_single(self, strategy: Type[bt.Strategy], params: dict = None) -> bt.MetaStrategy:
         if params is None:
             params = {}
+        else:
+            for k in params:
+                if type(params.get(k)) is range:  # it's kinda gay't
+                    self.logger.error("Params shouldn't be range.")
+                    exit()
 
         # todo multiple data feeds
-        self.cerebro.adddata(data)
+        self.cerebro.adddata(self.btdata)
         self.cerebro.addstrategy(strategy, **params)
         result = self.cerebro.run()[0]
 
         return result
 
-    def run_opt(self, data: pd.DataFrame, strategy: Type[bt.Strategy], params=None, wfo=False) -> List[List[bt.OptReturn]]:
+    def run_opt(self, strategy: Type[bt.Strategy],
+                params: dict = None, wfo: bool = False) -> List[List[bt.OptReturn]]:
         """
-        :param data:
         :param strategy:
         :param params:
         :param wfo: Whether walk forward optimization is active or not.
@@ -51,10 +62,51 @@ class StrategyOptimizer:
             params = {}
 
         if wfo:
-            results = None
+            results = self.walk_forward(strategy, params)
+
         else:
-            self.cerebro.adddata(data)
+            self.cerebro.adddata(self.btdata)
             self.cerebro.optstrategy(strategy, **params)
             results = self.cerebro.run()
 
         return results
+
+    def walk_forward(self, strategy, params):
+        tscv = TimeSeriesSplitImproved(10)
+        split = tscv.split(self.dataframe, fixed_length=True, train_splits=4, test_splits=1)
+        walk_forward_results = list()
+
+        for train, test in split:
+            datafeeds = {self.symbol: self.dataframe}
+            temp_cerebro = bt.Cerebro(stdstats=False, maxcpus=4)
+            temp_cerebro.broker.setcash(self.INIT_CASH)
+            temp_cerebro.broker.setcommission(commission=self.BINANCE_COMMISSION)
+            temp_cerebro.addanalyzer(AcctStats)
+            temp_cerebro.addanalyzer(bt.analyzers.PyFolio, _name='PyFolio')
+            temp_cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name="Basic_Stats")
+            temp_cerebro.addsizer(bt.sizers.PercentSizer, percents=70)
+
+            tester = deepcopy(temp_cerebro)
+
+            temp_cerebro.optstrategy(strategy, **params)
+
+            for s, df in datafeeds.items():
+                ddata = bt.feeds.PandasData(dataname=df.iloc[train], name=s, datetime='open_time')
+                temp_cerebro.adddata(ddata)
+
+            res = temp_cerebro.run()
+
+            opt_params = pd.DataFrame({r[0].params: r[0].analyzers.acctstats.get_analysis() for r in res}
+                                      ).T.loc[:, "return"].sort_values(ascending=False).index[0]._getpairs()
+
+            tester.addstrategy(strategy, **opt_params)
+            for s, df in datafeeds.items():
+                ddata = bt.feeds.PandasData(dataname=df.iloc[test], name=s, datetime='open_time')
+                tester.adddata(ddata)
+
+            res = tester.run()
+            res_dict = res[0].analyzers.acctstats.get_analysis()
+            res_dict["params"] = opt_params
+            walk_forward_results.append(res_dict)
+
+        return walk_forward_results
